1b4eb9373b67a8c42639af44424d5e75066bcc9f
github-insu <93insu@gmail.com>
2025-04-11 00:11:44 +0900

    chore: remove annotation that perform redundant functionality
    - bean registration: @configurationpropertiesscan(using scan), @enableconfigurationproperties(explicit registration)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication(scanBasePackages = "com.example")
-@ConfigurationPropertiesScan(basePackages = "com.example")
+//@ConfigurationPropertiesScan(basePackages = "com.example")
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
