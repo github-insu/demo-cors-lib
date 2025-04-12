b5aae3555d0930de736ca02f10bb371557529c0e
github-insu <93insu@gmail.com>
2025-04-12 23:40:10 +0900

    chore: add configuration for publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "com.example"
@@ -14,6 +15,31 @@ dependencies {
     compileOnly("org.springframework.boot:spring-boot-autoconfigure:3.4.4")
 
     implementation("org.springframework.boot:spring-boot-starter-web:3.4.4")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("cors-lib") {
+            from(components["java"])
+            groupId = "com.github.github-insu"
+            artifactId = project.name
+            version = project.version.toString()
+        }
+    }
+}
+
+tasks.publishToMavenLocal.configure {
+    // assemble 작업 후 publishToMavenLocal 실행
+    // assemble: 프로젝트의 아티팩트를 생성합니다. (jar 파일 등)
+    // publicshToMavenLocal: 빌드된 아티팩트를 로컬 저장소에 저장합니다. (.m2/repository)
+    dependsOn("assemble")
+}
+
+tasks.jar {
+    enabled = true
+    archiveClassifier.set("") // remove suffix "-plain"
+    // 다운로드 하려는 파일 이름: example-error-code-api-0.1.0.jar
+    // 위 명령 누락 때 파일 이름: example-error-code-api-0.1.0-plain.jar
 }
 
 tasks.test {
