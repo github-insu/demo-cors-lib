61f40cf2b393e0896c6655e5e84005d381d81426
github-insu <93insu@gmail.com>
2025-04-16 10:23:40 +0900

    chore: remove unused import statements

--- src/main/java/com/example/properties/MappedCorsProperties.java
+++ src/main/java/com/example/properties/MappedCorsProperties.java
@@ -2,8 +2,6 @@ package com.example.properties;
 
 import com.example.properties.allowed.CorsAllowedProperties;
 import com.example.properties.exposed.CorsExposedProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
 public record MappedCorsProperties(
         String path,
